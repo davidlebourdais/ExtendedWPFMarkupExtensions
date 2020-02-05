ecd3b847e960c15f83017305bc02c9d6c8b6c33d
David Le Bourdais <dlb.devx@gmail.com>
2020-02-05 23:43:45 +0100

    Created SingleBindingExtension

---
+++ Bindings/BindingHelper.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+using System.Xaml;
+using EMA.ExtendedWPFVisualTreeHelper;
+
+namespace EMA.ExtendedWPFMarkupExtensions.Utils
+{
+    /// <summary>
+    /// Provides some common methods for binding processings.
+    /// </summary>
+    public static class BindingHelper
+    {
+        /// <summary>
+        /// Gets a binding source from a name defined in the XAML scope.
+        /// </summary>
+        /// <param name="element_name">The name of the element to find.</param>
+        /// <param name="serviceProvider">A service provider from which a <see cref="IXamlNameResolver"/> can be retrieved.</param>
+        /// <returns>The nammed source object if any found, null otherwise.</returns>
+        public static object getSourceFromElementName(string element_name, IServiceProvider serviceProvider)
+        {
+            if (!string.IsNullOrWhiteSpace(element_name) && serviceProvider != null)
+            {
+                // Try to find element name in current xaml namescope:
+                var xnr = serviceProvider.GetService(typeof(IXamlNameResolver)) as IXamlNameResolver;
+                if (xnr != null)
+                    return xnr.Resolve(element_name);
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Gets a binding source as a <see cref="RelativeSource"/> of a <see cref="DependencyObject"/>.
+        /// </summary>
+        /// <param name="relativeSource"><see cref="RelativeSource"/> value for seeking.</param>
+        /// <param name="root">Root reference object.</param>
+        /// <returns>The source object that is relatated to the reference one, if any found, null otherwise.</returns>
+        public static object getSourceFromRelativeSource(RelativeSource relativeSource, DependencyObject root)
+        {
+            if (relativeSource == null) return root;
+
+            if (relativeSource.Mode == RelativeSourceMode.FindAncestor)
+            {
+                if (relativeSource.AncestorType != null)
+                    return WPFVisualFinders.FindParentByTypeExtended(root, relativeSource.AncestorType);
+                else if (relativeSource.AncestorLevel > 0)
+                    return WPFVisualFinders.FindParentByLevel(root, relativeSource.AncestorLevel);
+            }
+            else if (relativeSource.Mode == RelativeSourceMode.Self)
+            {
+                return root;
+            }
+            else if (relativeSource.Mode == RelativeSourceMode.PreviousData)
+            {
+                //TODO.
+                throw new NotSupportedException(nameof(RelativeSourceMode.PreviousData) + " is currently not supported.");
+            }
+            else if (relativeSource.Mode == RelativeSourceMode.TemplatedParent)
+            {
+                //TODO.
+                throw new NotSupportedException(nameof(RelativeSourceMode.TemplatedParent) + " is currently not supported.");
+            }
+            return root;
+        }
+
+        /// <summary>
+        /// Gets a binding source property value.
+        /// </summary>
+        /// <param name="source">A binding source object on which the property value will 
+        /// be retrieved (passed property path must be resolvable on this object's type).</param>
+        /// <param name="path">Path of the property where to find the value.</param>
+        /// <returns>The value of the property on the source object if any, null otherwise.</returns>
+        public static object GetBindingSourcePropertyValue(object source, PropertyPath path)
+        {
+            if (source == null) return null;
+
+            // Create a dummy watcher to resolve binding and get source property value:
+            using (var dummy = new DependencyPropertyWatcher<object>(source, path))
+            {
+                if (dummy != null)
+                    return dummy.Value;
+            }
+            return null;
+        }
+    }
+}
---
+++ Bindings/DependencyPropertyWatcher.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+
+namespace EMA.ExtendedWPFMarkupExtensions.Utils
+{
+    /// <summary>
+    /// A watcher for dependency property changes.
+    /// </summary>
+    /// <typeparam name="T">Type of the object to watch.</typeparam>
+    /// <remarks>From: https://www.engineeringsolutions.de/how-to-implement-a-dependencypropertywatcher/ </remarks>
+    public class DependencyPropertyWatcher<T> : DependencyObject, IDisposable
+    {
+        /// <summary>
+        /// Value of Value Property
+        /// </summary>
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register(nameof(Value), typeof(object), typeof(DependencyPropertyWatcher<T>), new PropertyMetadata(null, OnPropertyChanged));
+        /// <summary>
+        /// Called when Property Changes.
+        /// </summary>
+        public event EventHandler PropertyChanged;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DependencyPropertyWatcher{T}"/> class.
+        /// </summary>
+        /// <param name="relativesource">Databind relative source info</param>
+        /// <param name="propertyPath">Property path.</param>
+        public DependencyPropertyWatcher(RelativeSource relativesource, PropertyPath propertyPath)
+        {
+            Source = relativesource;
+            BindingOperations.SetBinding(this, ValueProperty, new Binding() { RelativeSource = relativesource, Path = propertyPath, Mode = BindingMode.OneWay });
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DependencyPropertyWatcher{T}"/> class.
+        /// </summary>
+        /// <param name="relativesource">Databind relative source info</param>
+        /// <param name="property_path">Path of Property</param>
+        public DependencyPropertyWatcher(RelativeSource relativesource, string property_path) : this(relativesource, new PropertyPath(property_path)) { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DependencyPropertyWatcher{T}"/> class.
+        /// </summary>
+        /// <param name="source">Databind source object</param>
+        /// <param name="propertyPath">Property path.</param>
+        public DependencyPropertyWatcher(object source, PropertyPath propertyPath)
+        {
+            Source = source;
+            BindingOperations.SetBinding(this, ValueProperty, new Binding() { Source = source, Path = propertyPath, Mode = BindingMode.OneWay });
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DependencyPropertyWatcher{T}"/> class.
+        /// </summary>
+        /// <param name="source">Databind source object</param>
+        /// <param name="property_path">Path of Property</param>
+        public DependencyPropertyWatcher(object source, string property_path) : this(source, new PropertyPath(property_path)) { }
+
+        /// <summary>
+        /// Gets the source object used.
+        /// </summary>
+        public object Source { get; private set; }
+
+        /// <summary>
+        /// Gets the current Value.
+        /// </summary>
+        public T Value
+        {
+            get { return (T)GetValue(ValueProperty); }
+        }
+
+        /// <summary>
+        /// Gets the current Value.
+        /// </summary>
+        public string PropertyPath
+        {
+            get { return BindingOperations.GetBindingExpression(this, ValueProperty).ResolvedSourcePropertyName; }
+        }
+
+        /// <summary>
+        /// Called when the Property is updated
+        /// </summary>
+        /// <param name="sender">Source</param>
+        /// <param name="args">Args</param>
+        public static void OnPropertyChanged(object sender, DependencyPropertyChangedEventArgs args)
+        {
+            var source = (DependencyPropertyWatcher<T>)sender;
+            source.PropertyChanged?.Invoke(source, EventArgs.Empty);
+        }
+
+        /// <summary>
+        /// Called when object is being disposed.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        /// <summary>
+        /// Called when object is being disposed.
+        /// </summary>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+                ClearValue(ValueProperty);
+        }
+    }
+}
